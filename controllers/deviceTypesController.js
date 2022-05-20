5f001c092338b6be6f909b2242cbab837574c164
schrockblock <elliot.schrock@gmail.com>
2022-05-20 12:18:21 -0400

    device type debugging

--- controllers/deviceTypesController.js
+++ controllers/deviceTypesController.js
@@ -4,10 +4,14 @@ var mongoose = require('mongoose');
 // Create endpoint /api/v1/device_types/:device_type_id for GET
 exports.getDeviceType = function(req, res) {
   DeviceType.findById(req.params.device_type_id, function(err, device_type){
-    if (err)
+    if (err) {
       res.send(err);
+      console.log(err);
+      return;
+    }
 
-  	console.log(JSON.stringify(req.params))
+    console.log(JSON.stringify(req.params));
+    console.log(JSON.stringify(device_type));
     res.json(device_type);
   });
 };
